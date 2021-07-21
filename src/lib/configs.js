e63d4033898ba58fccfa63beefd8fa135328f171
codeoneline <mutouji1@163.com>
2021-07-21 10:04:20 +0800

    config curveType should be string

--- src/lib/configs.js
+++ src/lib/configs.js
@@ -7,7 +7,7 @@ module.exports = {
       gasPrice: 0xba43b74000,
       gasLimit: 0x7a1200,
       chainId: 80001,
-      curveType: 1,
+      curveType: '1',
       deployedFile: 'maticTestnet.json',
       bip44: 0x800003c6,
       ownerSk: '18f81910df8ddfd5cc4d93e554805b5268a494bc1ff0d28f09be2025fb87984c',
