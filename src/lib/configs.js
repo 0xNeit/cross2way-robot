e1df8c29bfe46e1a6d938ed0a49a04fd36f8a547
codeoneline <mutouji1@163.com>
2022-01-07 18:21:42 +0800

    change config for movr

--- src/lib/configs.js
+++ src/lib/configs.js
@@ -155,7 +155,7 @@ module.exports = {
   'moonbeam': {
     'testnet': {
       chainType: 'DEV',
-      rpc: 'https://rpc.testnet.moonbeam.network',
+      rpc: 'https://rpc.api.moonbase.moonbeam.network',
       gasPrice: 0xba43b74000,
       gasLimit: 0x7a1200,
       chainId: 1287,
